f83a58ff6c4ddda512301a097bb5bf0c7b0b6358
harwi007 <106511301+harwi007@users.noreply.github.com>
2022-12-28 11:44:44 +0530

    Update install-prometheus.sh

--- install-prometheus.sh
+++ install-prometheus.sh
@@ -1,16 +1,16 @@
 sudo useradd --no-create-home prometheus
 sudo mkdir /etc/prometheus
 sudo mkdir /var/lib/prometheus
- 
-wget  https://github.com/prometheus/prometheus/releases/download/v2.37.5/prometheus-2.37.5.linux-amd64.tar.gz
-tar -xvf prometheus-2.23.0.linux-amd64.tar.gz
-sudo cp prometheus-2.23.0.linux-amd64/prometheus /usr/local/bin
-sudo cp prometheus-2.23.0.linux-amd64/promtool /usr/local/bin
-sudo cp -r prometheus-2.23.0.linux-amd64/consoles /etc/prometheus/
-sudo cp -r prometheus-2.23.0.linux-amd64/console_libraries /etc/prometheus
-sudo cp prometheus-2.23.0.linux-amd64/promtool /usr/local/bin/
 
-rm -rf prometheus-2.23.0.linux-amd64.tar.gz prometheus-2.19.0.linux-amd64
+wget  https://github.com/prometheus/prometheus/releases/download/v2.37.5/prometheus-2.37.5.linux-amd64.tar.gz
+tar -xvf prometheus-2.37.5.linux-amd64.tar.gz
+sudo cp prometheus-2.37.5.linux-amd64/prometheus /usr/local/bin
+sudo cp prometheus-2.37.5.linux-amd64/promtool /usr/local/bin
+sudo cp -r prometheus-2.37.5.linux-amd64/consoles /etc/prometheus/
+sudo cp -r prometheus-2.37.5.linux-amd64/console_libraries /etc/prometheus
+sudo cp prometheus-2.37.5.linux-amd64/promtool /usr/local/bin/
+
+rm -rf prometheus-2.37.5.linux-amd64.tar.gz prometheus-2.37.5.linux-amd64/
 sudo cp prometheus.yml /etc/prometheus/
 sudo cp prometheus.service /etc/systemd/system/prometheus.service
 
@@ -25,5 +25,3 @@ sudo systemctl daemon-reload
 sudo systemctl enable prometheus
 sudo systemctl start prometheus
 sudo systemctl status prometheus
-
-
